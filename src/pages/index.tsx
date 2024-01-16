90ce9de4c53bff984fba8abc7623853add5d84a4
Sahil Zambani <73684596+sahil-develops@users.noreply.github.com>
2024-01-16 23:59:18 +0530

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,111 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Select, { GroupBase, OptionProps, components } from 'react-select';
+import { JSX, useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+const inter = Inter({ subsets: ["latin"] });
+const CustomOption = (props: JSX.IntrinsicAttributes & OptionProps<unknown, boolean, GroupBase<unknown>>) => {
+  const data = props.data as { label: string, image: string, email: string };
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <components.Option {...props}>
+   <div style={{ display: 'flex', alignItems: 'center' }}>
+        <Image 
+          src={(props.data as { image: string }).image} // Path to the image, which should be a property in your options object
+          alt="Profile"
+          width={24} // Adjust the size as needed
+          height={24}
+          style={{ borderRadius: '50%', marginRight: '10px' }} // Make the image rounded and add margin
+        />
+        <div>
+          <div>{(props.data as { label: string }).label}</div> {/* This is where the name will go */}
+          <div style={{ fontSize: 'smaller' }}>{data.email}</div> {/* This is where the email will go */}
         </div>
       </div>
+    </components.Option>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+export default function Home() {
+  const [selectedOption, setSelectedOption] = useState(null);
+  const options = [
+    { value: "Mariana Augustine", label: "Mariana Augustine",image:"/zane.jpeg",email: 'mariana@example.com' },
+    { value: "Anita Gros", label: "Anita Gros",image:"/anita.jpeg",email: 'anita@example.com' },
+    { value: "Harvey Specter", label: "Harvey Specter",image:"/harvey.jpeg",email: 'harvey@example.com' },
+    { value: "Racheal Zane", label: "Racheal Zane",image:"/zane.jpeg",email: 'zane@example.com' },
+    { value: "Michael Jackson", label: "Michael Jackson",image:"/harvey.jpeg",email: 'michael@example.com' },
+    { value: "Louis Litt", label: "Louis Litt",image:"/littjpg.jpg",email: 'litt@example.com' },
+    { value: "David Warner", label: "David Warner",image:"/ross.jpg",email: 'david@example.com'},
+    { value: "Kevin Hart", label: "Kevin Hart",image:"/harvey.jpeg",email: 'kevin@example.com' },
+  ];
+  return (
+    <main
+      className={`flex gap-8 flex-col items-center justify-between p-24 ${inter.className}`}
+    >
+      <h1 className="text-5xl font-sans font-medium">Form Input</h1>
+      <Select
+        options={options}
+        className="w-1/2"
+        defaultValue={selectedOption}
+        placeholder="Select your value"
+        onChange={setSelectedOption}
+        components={{ Option: CustomOption}}
+        isMulti
+        isSearchable
+        dropdownIndicator={false}
+        noOptionsMessage={() => "No options found"}
+        styles={{
+          option: (provided, state) => ({
+            ...provided,
+            color: state.isSelected ? "white" : "black",
+            backgroundColor: state.isSelected ? "blue" : "white",
+            padding: 20,
+            borderBottom: "1px solid black",
+          }),
+          control: (provided, state) => ({
+            ...provided,
+            backgroundColor: "white",
+            border: "none",
+            borderRadius: 0,
+            borderBottom: "1px solid black",
+            boxShadow: "none",
+            "&:hover": {
+              border: "none",
+              borderBottom: "1px solid black",
+            },
+          }),
+          menu: (provided, state) => ({
+            ...provided,
+            width: "50%",
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          }),
+          clearIndicator: (provided, state) => ({
+            ...provided,
+            color: "white",
+            "&:hover": {
+              color: "white",
+            },
+          }),
+          multiValue: (provided, state) => ({
+            ...provided,
+            backgroundColor: "grey",
+            color: "white",
+            borderRadius: "30px",
+            padding: "2px 20px",
+          }),
+          multiValueLabel: (provided, state) => ({
+            ...provided,
+            color: "white",
+          }),
+          multiValueRemove: (provided, state) => ({
+            ...provided,
+            color: "white",
+            "&:hover": {
+              backgroundColor: "white",
+              color: "blue",
+            },
+          }),
+        }}
+      />
     </main>
-  )
+  );
 }
